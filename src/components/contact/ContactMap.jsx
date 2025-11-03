9e75f27814c9d81c404e24b8404b62e21fc75819
mmorin-novapros <mathieu.morin@novapros.ca>
2025-11-02 21:31:46 -0500

    feat: update contact map

--- src/components/contact/ContactMap.jsx
+++ src/components/contact/ContactMap.jsx
@@ -6,7 +6,7 @@ const ContactMap = () => {
             <div className="maps-area bg-gray overflow-hidden">
                 <div className="google-maps">
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10916.522537384646!2d-71.28308633052396!3d46.841118238074856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cb897331aac3039%3A0xe687196ad0e3c62f!2sNovaPros!5e0!3m2!1sfr!2sca!4v1724024978318!5m2!1sfr!2sca"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5457.49835750864!2d-71.2583439415472!3d46.84862729691993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cb8bd7ece88b55b%3A0x5dccc641f7cf3346!2s126%2053e%20Rue%20E%2C%20Qu%C3%A9bec%2C%20QC%20G1H%202H8!5e0!3m2!1sfr!2sca!4v1762135312290!5m2!1sfr!2sca"
                         width="600"
                         height="450"
                         allowFullScreen=""
