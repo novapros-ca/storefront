8432aaba5793fa60527bb4c6373bc61ab00255b0
mmorin-novapros <mathieu.morin@novapros.ca>
2025-11-05 11:11:18 -0500

    feat: update address in contact information

--- src/components/contact/ContactMap.jsx
+++ src/components/contact/ContactMap.jsx
@@ -6,7 +6,7 @@ const ContactMap = () => {
             <div className="maps-area bg-gray overflow-hidden">
                 <div className="google-maps">
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5457.49835750864!2d-71.2583439415472!3d46.84862729691993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cb8bd7ece88b55b%3A0x5dccc641f7cf3346!2s126%2053e%20Rue%20E%2C%20Qu%C3%A9bec%2C%20QC%20G1H%202H8!5e0!3m2!1sfr!2sca!4v1762135312290!5m2!1sfr!2sca"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3595.6955638940976!2d-71.25605042282189!3d46.84862367113048!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cb8bd7ece88db85%3A0x1cc8619f911241fe!2s128%2053e%20Rue%20E%2C%20Qu%C3%A9bec%2C%20QC%20G1H%202H8%2C%20Canada!5e1!3m2!1sfr!2sus!4v1762358997947!5m2!1sfr!2sus"
                         width="600"
                         height="450"
                         allowFullScreen=""
--- src/components/header/HeaderTopStyle1.jsx
+++ src/components/header/HeaderTopStyle1.jsx
@@ -10,7 +10,7 @@ const HeaderTopStyle1 = () => {
                         <div className="col-xl-6 col-lg-8 offset-xl-3 pl-30 pl-md-15 pl-xs-15">
                             <ul className="item-flex">
                                 <li>
-                                    <a target="_blank" href="https://maps.app.goo.gl/z9eyKD5zcK33aD3x7"><i className="fas fa-map-marker-alt"></i> 126 53e rue Est, Québec, QC</a>
+                                    <a target="_blank" href="https://maps.app.goo.gl/c33hyLZEzpSxSpi78"><i className="fas fa-map-marker-alt"></i> 128 53e rue Est, Québec, QC</a>
                                 </li>
                                 <li>
                                     <a href="tel:4188007784"><i className="fas fa-phone-alt"></i>(418) 800-7784</a>
